109977b1f02e12e0f229086d2478d568156fc6c9
alexgatu <alexandru.gatu@gmail.com>
2024-10-03 21:19:20 +0300

    added prime numbers solution

--- src/Hello.java
+++ src/Hello.java
@@ -55,7 +55,18 @@ public class Hello {
             System.out.println(arg);
         }
 
-
+        for (int i=2; i<=1000000;i++) {
+            boolean isPrime = true;
+            for (int j=2; j <= i/2 ; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                System.out.println("Number " + i + " is prime.");
+            }
+        }
 
     }
 
